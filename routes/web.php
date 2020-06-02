06f3e494f07626af1333896e7f9fe3a2d690bba8
ekusuy26 <y.yuu6221@gmail.com>
2020-06-02 21:30:58 +0900

    set routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/rock-paper-scissors', 'GameController@index')->name('rock-paper-scissors');
