95b3c5247d9dbf279a47e0eefc230d3d971e9af2
ekusuy26 <y.yuu6221@gmail.com>
2020-06-03 21:11:10 +0900

    Changed to be able to send form

--- resources/views/rockPaperScissors.blade.php
+++ resources/views/rockPaperScissors.blade.php
@@ -1,6 +1,7 @@
 <form action="/rock-paper-scissors" method="post">
-  <input type="radio" name="test" value="rock">ぐー
-  <input type="radio" name="test" value="scissors">ちょき
-  <input type="radio" name="test" value="paper">ぱー
+  @csrf
+  <input type="radio" name="hand" value="rock">ぐー
+  <input type="radio" name="hand" value="scissors">ちょき
+  <input type="radio" name="hand" value="paper">ぱー
   <input type="submit" value="じゃんけん">
 </form>
