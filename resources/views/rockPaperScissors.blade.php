0ee8eb390c1c278f555aac8bc68e3083a8f716c5
ekusuy26 <y.yuu6221@gmail.com>
2020-06-03 20:37:57 +0900

    Create submit button

--- resources/views/rockPaperScissors.blade.php
+++ resources/views/rockPaperScissors.blade.php
@@ -1,3 +1,4 @@
 <input type="radio" name="test" value="rock">ぐー
 <input type="radio" name="test" value="scissors">ちょき
 <input type="radio" name="test" value="paper">ぱー
+<input type="submit" value="じゃんけん">
