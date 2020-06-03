c695fbc7cdecbd7e4b2d607ecd77564b565ed7f4
ekusuy26 <y.yuu6221@gmail.com>
2020-06-03 21:54:01 +0900

    Confirm win/loss with controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -12,7 +12,32 @@ class GameController extends Controller
     }
     public function resultRockPaperScissors(Request $request)
     {
-        dd($request);
-        return view('rockPaperScissors');
+        $myHand = $request->hand;
+        $opponent = array('rock','paper','scissors');
+        // $key = array_rand($opponent);
+        // $opponentHand = $opponent[$key];
+        $opponentHand = array_rand($opponent);
+        if ($myHand == 0 && $opponentHand ==0){
+            $result = 'あいこ';
+        } elseif ($myHand == 0 && $opponentHand ==1){
+            $result = 'まけ';
+        } elseif ($myHand == 0 && $opponentHand ==2){
+            $result = 'かち';
+        } elseif ($myHand == 1 && $opponentHand ==0){
+            $result = 'かち';
+        } elseif ($myHand == 1 && $opponentHand ==1){
+            $result = 'あいこ';
+        } elseif ($myHand == 1 && $opponentHand ==2){
+            $result = 'まけ';
+        } elseif ($myHand == 2 && $opponentHand ==0){
+            $result = 'まけ';
+        } elseif ($myHand == 2 && $opponentHand ==1){
+            $result = 'かち';
+        } elseif ($myHand == 2 && $opponentHand ==2){
+            $result = 'あいこ';
+        }else{
+            $result = 'エラー';
+        }
+        return view('rockPaperScissors', compact('result', 'myHand', 'opponentHand'));
     }
 }
--- resources/views/rockPaperScissors.blade.php
+++ resources/views/rockPaperScissors.blade.php
@@ -1,7 +1,7 @@
 <form action="/rock-paper-scissors" method="post">
   @csrf
-  <input type="radio" name="hand" value="rock">ぐー
-  <input type="radio" name="hand" value="scissors">ちょき
-  <input type="radio" name="hand" value="paper">ぱー
+  <input type="radio" name="hand" value="0">ぐー
+  <input type="radio" name="hand" value="1">ぱー
+  <input type="radio" name="hand" value="2">ちょき
   <input type="submit" value="じゃんけん">
 </form>
