d45d5c497c181d36f861ee8e9ef683c9da631ae2
ekusuy26 <y.yuu6221@gmail.com>
2020-06-03 20:40:03 +0900

    Create a data transmission form

--- resources/views/rockPaperScissors.blade.php
+++ resources/views/rockPaperScissors.blade.php
@@ -1,4 +1,6 @@
-<input type="radio" name="test" value="rock">ぐー
-<input type="radio" name="test" value="scissors">ちょき
-<input type="radio" name="test" value="paper">ぱー
-<input type="submit" value="じゃんけん">
+<form action="#" method="post">
+  <input type="radio" name="test" value="rock">ぐー
+  <input type="radio" name="test" value="scissors">ちょき
+  <input type="radio" name="test" value="paper">ぱー
+  <input type="submit" value="じゃんけん">
+</form>
