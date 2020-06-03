0984ac3e8af41220d810ee7f889135783b1703f3
ekusuy26 <y.yuu6221@gmail.com>
2020-06-03 22:17:33 +0900

    Completed for the time being

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,3 +1,16 @@
 {{$result}}
-{{$myHand}}
-{{$opponentHand}}
+@if ($myHand == 0)
+ぐー
+@elseif ($myHand == 1)
+ぱー
+@else
+ちょき
+@endif
+
+@if ($opponentHand == 0)
+ぐー
+@elseif ($opponentHand == 1)
+ぱー
+@else
+ちょき
+@endif
